ca11553dadf6d23b4b771748139fc19198e10ac5
ddegremont <ddegremont@gmail.com>
2018-05-16 23:45:28 +0200

    delete source
    delete source

--- SOURCES/includes/social.php
+++
@@ -1,8 +0,0 @@
-<div class="social-icons">
-  <ul>
-    <li class="facebook"><a href="https://www.facebook.com/uneanneedevoyages/" target="_blank"><i class="fa fa-facebook"></i></a></li>
-    <li class="twitter"><a href="https://twitter.com/1AnneeDeVoyages" target="_blank"><i class="fa fa-twitter"></i></a></li>
-    <li class="instagram"><a href="https://www.instagram.com/uneanneedevoyages/" target="_blank"><i class="fa fa-instagram"></i></a></li>
-    <li class="youtube"><a href="https://www.youtube.com/channel/UCESfTu2chDpHfGFkx-cfKDQ" target="_blank"><i class="fa fa-youtube"></i></a></li>
-  </ul>
-</div>
