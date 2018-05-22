c7e9edbad7a9632d880a52f925cf825a3cb9aabf
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-22 16:54:43 +0200

    connection.php done.

---
+++ g4/src/functions/connection/connection.php
@@ -0,0 +1,15 @@
+<?php
+function connection()
+{
+    $host       = "localhost";
+    $port       = "3306";
+    $dbname     = "perfect_website";
+    $username   = "root";
+    $password   = "root";
+    try {
+        $connection = new PDO("mysql:host=" . $host . ";dbname=" . $dbname . ";port=" . $port, $username, $password);
+    } catch(Exception $exception) {
+        echo "Fatal error: couldn't not connect to the database.";
+        exit;
+    }
+}
