36b99ab6bd57cfdad8763b3fd6c39748e06f21af
Draktors <enzo.hespel@hotmail.fr>
2018-05-22 10:53:00 +0200

    initial commit with rollup.js

---
+++ g1/rollup.config.js
@@ -0,0 +1,21 @@
+import sass from 'rollup-plugin-sass';
+import url from "rollup-plugin-url";
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+export default {
+    input: 'src/js/app.js',
+    output: {
+        file: 'public/app.generated.js',
+        format: 'umd'
+    },
+    sourceMap: true,
+    plugins: [
+        sass(),
+        url({
+            limit: 10 * 1024, // inline files < 10k, copy files > 10k
+            include: ["**/*.svg","**/*.png","**/*.jpg","**/*.mov","**/*.gif","**/*.mp3"], // defaults to .svg, .png, .jpg and .gif files
+            emitFiles: true // defaults to true
+        }),
+        sourcemaps(),
+    ],
+};
