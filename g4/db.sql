da06e3e439e1ae05d19b88da929cc743be483a01
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-22 16:34:24 +0200

    Database V0.0.0 (previous commit had no dump.

--- g4/db.sql
+++ g4/db.sql
@@ -14,6 +14,158 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `companies`
+--
+
+DROP TABLE IF EXISTS `companies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `companies` (
+  `name` varchar(64) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `companies`
+--
+
+LOCK TABLES `companies` WRITE;
+/*!40000 ALTER TABLE `companies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `companies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `factsheets`
+--
+
+DROP TABLE IF EXISTS `factsheets`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `factsheets` (
+  `title` varchar(64) NOT NULL,
+  `imgSrc` varchar(255) NOT NULL,
+  `imgAlt` varchar(64) NOT NULL,
+  `paragraph` text NOT NULL,
+  `category` varchar(64) NOT NULL,
+  `websiteUrl` varchar(255) NOT NULL,
+  PRIMARY KEY (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `factsheets`
+--
+
+LOCK TABLES `factsheets` WRITE;
+/*!40000 ALTER TABLE `factsheets` DISABLE KEYS */;
+/*!40000 ALTER TABLE `factsheets` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `imagebox`
+--
+
+DROP TABLE IF EXISTS `imagebox`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `imagebox` (
+  `title` varchar(64) NOT NULL,
+  `subtitle` varchar(255) NOT NULL,
+  `imgSrc` varchar(255) NOT NULL,
+  `imgAlt` varchar(64) NOT NULL,
+  PRIMARY KEY (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `imagebox`
+--
+
+LOCK TABLES `imagebox` WRITE;
+/*!40000 ALTER TABLE `imagebox` DISABLE KEYS */;
+/*!40000 ALTER TABLE `imagebox` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `news`
+--
+
+DROP TABLE IF EXISTS `news`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `news` (
+  `title` varchar(64) NOT NULL,
+  `subtitle` varchar(255) NOT NULL,
+  `imgSrc` varchar(255) NOT NULL,
+  `imgAlt` varchar(64) NOT NULL,
+  `paragraph` text NOT NULL,
+  `signature` varchar(255) NOT NULL,
+  `logoSrc` varchar(255) NOT NULL,
+  `logoAlt` varchar(64) NOT NULL,
+  `newscol` varchar(45) NOT NULL,
+  PRIMARY KEY (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partners`
+--
+
+DROP TABLE IF EXISTS `partners`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `partners` (
+  `name` varchar(64) NOT NULL,
+  `logoSrc` varchar(255) NOT NULL,
+  `logoAlt` varchar(64) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partners`
+--
+
+LOCK TABLES `partners` WRITE;
+/*!40000 ALTER TABLE `partners` DISABLE KEYS */;
+/*!40000 ALTER TABLE `partners` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `top`
+--
+
+DROP TABLE IF EXISTS `top`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `top` (
+  `title` varchar(64) NOT NULL,
+  `subtitle` varchar(64) NOT NULL,
+  `category` varchar(64) NOT NULL,
+  PRIMARY KEY (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `top`
+--
+
+LOCK TABLES `top` WRITE;
+/*!40000 ALTER TABLE `top` DISABLE KEYS */;
+/*!40000 ALTER TABLE `top` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +176,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-22 15:22:06
+-- Dump completed on 2018-05-22 16:32:31
