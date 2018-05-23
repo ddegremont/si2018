b999f75ad251c6ca4f966f91d0726050122c47f5
SophieChieko <smemeint@gmail.com>
2018-05-23 18:46:38 +0200

    Page articles

--- g1/gulpfile.js
+++ g1/gulpfile.js
@@ -3,19 +3,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 
-gulp.task('build', ['sass']);
-
-gulp.task('sass', function () {
-    return gulp.src('src/style/main.sass')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/css/'));
-});
-gulp.task('reset', function () {
-    return gulp.src('src/style/reset.sass')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/css/'));
+gulp.task('sass', function(){
+    return gulp.src('src/style//*.sass')
+        .pipe(sass())
+        .pipe(gulp.dest('public/css'))
 });
 
-gulp.task('default', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('watch', function(){
+    gulp.watch('src/style//*.sass', ['sass']);
+    // autres observations
 });
