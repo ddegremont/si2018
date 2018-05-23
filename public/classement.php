caa24b8b348ba88809996b6c61bac284a0542a91
alexandretomasmartins <alexandre.martins@hetic.net>
2018-05-23 15:08:22 +0200

    Integration begins

---
+++ public/classement.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+    <link rel="stylesheet" href="styles/styles.css">
+    <title>Une année de voyages</title>
+</head>
+<body>
+
+  <section class="Top">
+
+    <!-- Header -->
+    <header class="Header">
+      <div class="Logo">
+        <img class="Logo__img" src="img/logo.png" alt="Logo">
+        <span class="Logo__name"></span>
+      </div>
+      <div class="Menu">
+        <p class="Menu__text">Menu</p>
+      </div>
+    </header>
+    <!-- Header -->
+
+    <!-- Top Titles -->
+    <div class="Top__titles">
+      <h2 class="Top__hotelTitle">1 ENVIE.</h2>
+      <h2 class="Top__hotelTitle">100 HÔTELS.</h2>
+    </div>
+    <!-- Top Titles -->
+
+  </section>
+
+<script type="text/javascript" src="./js/main.js">
+
+</script>
+</body>
+</html>
