4f4e87163e394cfc9fe93e043e0021ad7c66d707
PierreTurnbull <pierre.turnbull42@gmail.com>
2018-05-24 10:18:03 +0200

    Index and news PHP/Integration merge done.

--- g4/src/functions/displayContent/displayNewsArticleContent.php
+++ g4/src/functions/displayContent/displayNewsArticleContent.php
@@ -3,19 +3,23 @@ function displayNewsArticleContent($data)
 {
     $imgPath = "src/img/";
     ?>
-    <article class="news_articles_item">
-        <h2 class="news_articles_item_title"><?= $data["title"] ?></h2>
-        <h3 class="news_articles_item_subtitle"><?= $data["subtitle"] ?></h3>
-        <h4 class="news_articles_item_company"><?= $data["company"] ?></h4>
-        <img src="<?= $imgPath . $data["imgSrc"] ?>" alt="<?= $data["imgAlt"] ?>">
-        <p class="news_articles_item_subtitle"><?= $data["paragraph"] ?></p>
-        <p class="news_articles_item_signature"><?= $data["signature"] ?></p>
-        <img src="<?= $imgPath . $data["logoSrc"] ?>" alt="<?= $data["logoAlt"] ?>">
-        <a href="newsArticle.php?id=<?= $data["id"] ?>">see article</a>
-        <form action="src/actions/newsArticle_doDelete.php" method="POST">
-            <input value="<?= $data["id"] ?>" name="id" title="id" style="display:none">
-            <input type="submit" value="Delete">
-        </form>
-    </article>
+    <div class="newsairJobContainer">
+        <div class="newsairJobBox">
+            <div>
+                <img src="<?= $imgPath . $data["imgSrc"] ?>" alt="<?= $data["imgAlt"] ?>">
+            </div>
+            <h2 class="news_articles_item_title"><?= $data["title"] ?></h2>
+            <p class="news_articles_item_subtitle"><?= $data["paragraph"] ?></p>
+            <h3 class="news_articles_item_company"><?= $data["company"] ?></h3>
+            <div>
+                <img src="<?= $imgPath . $data["logoSrc"] ?>" alt="<?= $data["logoAlt"] ?>">
+            </div>
+            <a href="newsArticle.php?id=<?= $data["id"] ?>">see article</a>
+            <form action="src/actions/newsArticle_doDelete.php" method="POST">
+                <input value="<?= $data["id"] ?>" name="id" title="id" style="display:none">
+                <input type="submit" value="Delete">
+            </form>
+        </div>
+    </div>
     <?php
 }
