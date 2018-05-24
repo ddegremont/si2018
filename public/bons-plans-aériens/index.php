fd458064767ccdc4fd0d1e622b926c4725a534d6
alexandretomasmartins <alexandre.martins@hetic.net>
2018-05-24 10:17:55 +0200

    Ajout des bons plans aériens

---
+++ public/bons-plans-aériens/index.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+    <link rel="stylesheet" href="../styles/styles.css">
+    <title>Une année de voyages</title>
+</head>
+<body>
+
+  <section class="Top" style="background-image: url('../img/bons-plans.jpg');">
+
+    <!-- Header -->
+    <header class="Header">
+      <div class="Logo">
+        <img class="Logo__img" src="../img/logo.png" alt="Logo">
+        <span class="Logo__name"></span>
+      </div>
+      <div class="Menu">
+        <img class="Menu__img" src="../img/burger_menu.svg" alt="">
+      </div>
+    </header>
+    <!-- Header -->
+
+    <!-- Top Titles -->
+    <div class="Top__titles">
+      <h2 class="Top__hotelTitle">BONS PLANS</h2>
+      <h2 class="Top__hotelTitle">A&Eacute;RIENS.</h2>
+    </div>
+    <!-- Top Titles -->
+
+  </section>
+
+  <section class="Plugs">
+
+    <div class="Container">
+        <div class="Container__plug">
+          <div class="Container__imgContainer">
+            <img class="Container__img" src="../img/bon-plan-maroc.jpg" alt="">
+            <h3 class="Container__countryTitle">Ryanair</h3>
+            <div class="Container__cityTitleContainer">
+              <p class="Container__paragraph">Réservez votre billet d'ici le 23 mai avec Ryanair et bénéficiez de jusqu'à <span class="Container__boldParagraph">30% de réduction</span> sur les vols à destination de Palerme et de Marrakech.</p>
+            </div>
+              <div class="Container__articleLinkContainer">
+                <a class="Container__articleLink" href="plug.php">Voir l'article <span class="Container__arrow">▶</span></a>
+              </div>
+          </div>
+        </div>
+      <div class="Container__plug">
+        <div class="Container__imgContainer">
+          <img class="Container__img" src="../img/bon-plan-turquie.jpg" alt="">
+          <h3 class="Container__countryTitle">Pegasus Airlines</h3>
+          <div class="Container__cityTitleContainer">
+              <p class="Container__paragraph">Partez cet automne en Turquie avec Pegasus Airlines et profitez de jusqu'à <span class="Container__boldParagraph">30% de réduction</span> sur votre vol.</p>
+          </div>
+            <div class="Container__articleLinkContainer">
+                <a class="Container__articleLink" href="plug.php">Voir l'article <span class="Container__arrow">▶</span></a>
+            </div>
+        </div>
+      </div>
+      <div class="Container__plug">
+        <div class="Container__imgContainer">
+          <img class="Container__img" src="../img/bon-plan-malte.jpg" alt="">
+            <h3 class="Container__countryTitle">Air Malta</h3>
+          <div class="Container__cityTitleContainer">
+              <p class="Container__paragraph">Envolez-vous à Malte avec Air Malta et bénéficiez de jusqu'à <span class="Container__boldParagraph">10% de réductions</span> si vous partez de Paris ou Lyon…</p>
+          </div>
+            <div class="Container__articleLinkContainer">
+                <a class="Container__articleLink" href="plug.php">Voir l'article <span class="Container__arrow">▶</span></a>
+            </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+
+<script type="text/javascript" src="../js/main.js">
+
+</script>
+</body>
+</html>
---
+++ public/bons-plans-aériens/plug.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
+    <link rel="stylesheet" href="../styles/styles.css">
+    <title>Une année de voyages</title>
+</head>
+<body>
+
+  <section class="Oneplug">
+
+    <div class="Oneplug__header" style="background-image: url('../img/bon-plan-maroc-opacity.jpg');">
+      <div class="Oneplug__headerTextContainer">
+        <h3 class="Oneplug__headerCountryTitle">Ryanair</h3>
+        <h2 class="Oneplug__headerCityTitle">-30% SUR LES VOLS.</h2>
+      </div>
+      <div class="Oneplug__closeIconContainer">
+        <a href="index.php"><img class="Oneplug__closeIcon" src="../img/close.svg" alt="Close icon"></a>
+      </div>
+    </div>
+
+    <div class="Oneplug__content">
+      <div class="Oneplug__contentParagraph">
+        <div class="Oneplug__line"></div>
+        <p class="Oneplug__paragraph">
+            Réservez votre billet d’ici avant le 23 mai avec Ryanair et bénéficiez jusqu’à 30% de réduction sur les vols à destination de Palerme et Marrakech.
+        </p>
+      </div>
+      <div class="Oneplug__iconsContainer">
+        <div class="Oneplug__iconsLine">
+          <div class="Oneplug__iconBox">
+            <img class="Oneplug__icon" src="../img/hourglass.svg" alt="Hourglass icon">
+            <p class="Oneplug__text">Jusqu'au 14 juin 2018.</p>
+          </div>
+          <div class="Oneplug__iconBox">
+            <img class="Oneplug__icon" src="../img/calendar.svg" alt="Calendar icon">
+            <p class="Oneplug__text">Vols de juin à septembre.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+<script type="text/javascript" src="../js/main.js">
+
+</script>
+</body>
+</html>
